baafafd0b5a4826c9501073908283348824a14d8
renweibo <renweibo@gmail.com>
2022-04-27 15:15:40 +0000

    做些微调整

--- ati18n/__init__.py
+++ ati18n/__init__.py
@@ -1,5 +1,9 @@
 """Top-level package for ati18n."""
 
+__author__ = """Weibo Ren"""
+__email__ = 'renweibo@gmail.com'
+__version__ = '0.1.0'
+
 OUTPUT_HEAD = {'tools':'ati18n', 'version':'0.1.0', 'app_type':'java'}
 ERROR_1001 = {'no':'1001', 'level':'错误', 'scope':'整体翻译', 'name':'翻译条目数不匹配', 'data':'检查的数据文件和相关信息', "comment":''}
 ERROR_1002 = {'no':'1002', 'level':'错误', 'scope':'整体翻译', 'name':'文件名指定的语言种类和文件内容不匹配', 'data':'检查的数据文件和相关信息', "comment":''}
