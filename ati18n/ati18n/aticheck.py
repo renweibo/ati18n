6e87318177369ed5c7c19e95b45515d6544c8d10
painterg <22396997@qq.com>
2022-04-30 06:38:50 +0000

    completed check 1002 and 2001,fixed bug from 1001

--- ati18n/ati18n/aticheck.py
+++ ati18n/ati18n/aticheck.py
@@ -11,6 +11,12 @@ import os
 
 class BaseCheck:
 
+    count_status = []
+    lang_status = []
+    check_1001 = True
+    check_1002 = True
+    check_2001 = True
+
     """ 抽取文件数据抽取成字典格式：把每份文件的内容抽取成字典表 """
     def extract_dict(self, f):
         pass
@@ -20,19 +26,14 @@ class BaseCheck:
     def output_result(self, df):
         pass
 
-    """ check errors about the type of 1001 return list"""
-    def check_1001(self, app_type, data):
-        return []
+    """ check errors about the type of 1001 and 1002"""
+    def check_file(self, app_type, file_name, value):
+        return None
 
 
-    """ check errors about the type of 1002 return list"""
-    def check_1002(self, app_type, data):
-        return []
-
-
-    """ check errors about the type of 2001 return list"""
-    def check_2001(self, app_type, data):
-        return []
+    """ check errors about the type of 1003"""
+    def check_item(self, app_type, file_name, value):
+        return None
 
     """ 
         path：i18n文件路径，不允许有子目录
@@ -40,10 +41,17 @@ class BaseCheck:
     """
     def check(self,app_type, path, regex):
         df = self.load_file(path, regex)
-        data1 = self.check_1001(app_type, df)
-        data2 = self.check_1002(app_type, df)
-        data3 = self.check_2001(app_type, df)
-        data = data1 + data2 + data3
+        data = []
+        ''' 获取df中每一行的数据，name为待翻译的key，value为行数据 '''
+        for row in df.iterrows():
+            key = row[0]
+            value = row[1]
+            ''' item为每一行的列数据，item[0]为列头，item[1]为列值 '''
+            for item in value.items():
+                data1 = self.check_file(app_type, item[0], item[1])
+                data2 = self.check_item(app_type, item[0], key, item[1])
+                if data1 is not None: data.append(data1)
+                if data2 is not None: data.append(data2)
         self.output_result(data)        
 
 
@@ -66,7 +74,6 @@ class BaseCheck:
         return df.to_json(force_ascii=False)
 
 
-
 class CheckJava(BaseCheck):
 
     def extract_dict(self, f):
@@ -79,38 +86,24 @@ class CheckJava(BaseCheck):
         return properties
 
     
-    def check_1001(self, app_type, data):
-        results = []
-        ''' 获取df中每一行的数据，name为待翻译的key，value为行数据 '''
-        for row in data.iterrows():
-            name = row[0]
-            value = row[1]
-            ''' item为每一行的列数据，item[0]为列头，item[1]为列值 '''
-            for item in value.items():
-                file_name = item[0]
-                v = item[1]
-                if len(v) == 0:
-                    data = OutputDataSimple(type=DataType.File.value, file_path=item[0])
-                    result = OutputResult(No=ERROR_1001['no'], Level=ERROR_1001['level'], Scope=ERROR_1001['scope'], Name=ERROR_1001['name'], Data=data.json, Comment=ERROR_1001['comment'])
-                    results.append(result.json)
-        return results
+    def check_file(self, app_type, file_name, value):
+        result = None
+        if self.check_file and len(value) == 0  and self.count_status.count(file_name) == 0:
+            self.count_status.append(file_name)
+            result = out_template_file(file_name, ERROR_1001)
+        elif self.check_1002 and len(value) > 0 and not determine_lang(file_name, value, app_type)[0] and self.lang_status.count(file_name) == 0:
+            self.lang_status.append(file_name)
+            result = out_template_file(file_name, ERROR_1002)
+        return result.json if result is not None else None
 
 
-    def check_1002(self, app_type, data):
-        results = []
-        for row in data.iterrows():
-            name = row[0]
-            value = row[1]
-            for item in value.items():
-                file_name = item[0]
-                v = item[1]
-                if len(v) > 0:
-                    if not determine_lang(file_name, v, app_type):
-                        data = OutputDataSimple(type=DataType.File.value, file_path=item[0])
-                        result = OutputResult(No=ERROR_1002['no'], Level=ERROR_1002['level'], Scope=ERROR_1002['scope'], Name=ERROR_1002['name'], Data=data.json, Comment=ERROR_1002['comment'])
-                        results.append(result.json)
-        return results
-
+    def check_item(self, app_type, file_name, key, value):
+        result = None
+        if self.check_2001 and len(value) > 0:
+            lang_type = determine_lang(file_name, value, app_type)
+            if not lang_type[0]:
+                result = out_template_item(file_name, ERROR_2001, key, value, lang_type[1])
+        return result.json if result is not None else None
 
     def output_result(self, data):
         app_path = os.getcwd()
--- ati18n/ati18n/utils.py
+++ ati18n/ati18n/utils.py
@@ -3,6 +3,7 @@ __email__ = '22396997@qq.com'
 
 import py3langid as langid
 import time, datetime, pytz
+from .common import *
 
 def get_now():
         utc = pytz.timezone('Asia/Shanghai')
@@ -19,6 +20,19 @@ def determine_lang(file_name, value, app_type):
         start = end - len(lang_type)
         type_from_file_name=file_name[start:end]
         if (lang_type == type_from_file_name):
-            return True
+            return (True, lang_type)
         else:
-            return False
+            return (False, lang_type)
+
+
+def out_template_file(file_name, error):
+    data = OutputDataSimple(type=DataType.File.value, file_path=file_name)
+    result = OutputResult(No=error['no'], Level=error['level'], Scope=error['scope'], Name=error['name'], Data=data.json, Comment=error['comment'])
+    return result
+
+
+def out_template_item(file_name, error, key, value, code):
+    data = OutputDataSimple(type=DataType.File.value, file_path=file_name)
+    data = OutputDataItem(type=DataType.Item.value, file_path=file_name, key_name=key, key_value=value, lang_code=code, info='')
+    result = OutputResult(No=error['no'], Level=error['level'], Scope=error['scope'], Name=error['name'], Data=data.json, Comment=error['comment'])
+    return result
