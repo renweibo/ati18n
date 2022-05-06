e075dbf9139dc4a98e87b34f1fb33b0cf0a8c01b
painterg <22396997@qq.com>
2022-05-06 11:09:06 +0800

    fixed lint cli

--- ati18n/cli.py
+++ ati18n/cli.py
@@ -17,7 +17,7 @@ class APPType(str, Enum):
 @app.command()
 def check(path: Path = typer.Argument(..., help="应用路径"), 
     app_type: APPType = typer.Option("Java", "--type", "-t", help="specify the app type. 指定应用类型")):
-    
+
     """Console script for ati18n.
 
     用来检查国际化是否有问题或可疑点的工具，支持Java应用、Vue应用、Flask应用中的多语言功能，检查其中是否有些可能的问题点，方便研发人员快速解决问题。"""
@@ -30,4 +30,3 @@ def check(path: Path = typer.Argument(..., help="应用路径"),
 
 if __name__ == "__main__":
     app()
-    
